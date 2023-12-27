d5a9c75efbfff4f20d512504b1b36b5166332b04
emre.kartal <emre.kartal@etu.uca.fr>
2023-12-27 19:44:26 +0100

    Add a framework projet for the DI

--- Sources/AllInApp/AllIn/AllInApp.swift
+++ Sources/AllInApp/AllIn/AllInApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import DependencyInjection
 
 @main
 struct AllInApp: App {
--- Sources/AllInApp/AllIn/ContentView.swift
+++ Sources/AllInApp/AllIn/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import DependencyInjection
 
 struct ContentView: View {
     
