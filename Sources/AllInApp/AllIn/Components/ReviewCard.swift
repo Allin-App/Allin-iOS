f98bca02d61768826e8fa58b1172d72f37fd1b65
ludelanier <lucas.delanier@etu.uca.fr>
2024-01-10 14:00:24 +0100

    start historique page

---
+++ Sources/AllInApp/AllIn/Components/ReviewCard.swift
@@ -0,0 +1,18 @@
+//
+//  ReviewCard.swift
+//  AllIn
+//
+//  Created by Lucas Delanier on 10/01/2024.
+//
+
+import SwiftUI
+
+struct ReviewCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReviewCard()
+}
---
+++ Sources/AllInApp/AllIn/Views/HistoricBetView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoricBetView.swift
+//  AllIn
+//
+//  Created by Lucas Delanier on 10/01/2024.
+//
+
+import SwiftUI
+
+struct HistoricBetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoricBetView()
+}
