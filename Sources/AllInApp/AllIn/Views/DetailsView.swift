f12e9a0407f3d082bb0cb2ffde019b3f6c889cb1
ludelanier <lucas.delanier@etu.uca.fr>
2024-01-12 16:19:24 +0100

    start detail page with animation

---
+++ Sources/AllInApp/AllIn/Views/DetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsView.swift
+//  AllIn
+//
+//  Created by Lucas Delanier on 12/01/2024.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsView()
+}
