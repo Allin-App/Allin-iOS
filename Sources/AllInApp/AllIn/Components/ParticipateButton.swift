57786190c8c47596c3e747d2b704312d96acb4b7
ludelanier <lucas.delanier@etu.uca.fr>
2024-01-15 16:51:31 +0100

    new logic for open the modal

---
+++ Sources/AllInApp/AllIn/Components/ParticipateButton.swift
@@ -0,0 +1,18 @@
+//
+//  ParticipateButton.swift
+//  AllIn
+//
+//  Created by Lucas Delanier on 15/01/2024.
+//
+
+import SwiftUI
+
+struct ParticipateButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ParticipateButton()
+}
