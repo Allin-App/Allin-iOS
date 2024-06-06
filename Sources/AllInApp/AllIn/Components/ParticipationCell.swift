560ee2f4c53bea3d8ae8753405781aa87f17ecc6
ludelanier <lucas.delanier@etu.uca.fr>
2024-06-06 10:17:39 +0200

    merge

---
+++ Sources/AllInApp/AllIn/Components/ParticipationCell.swift
@@ -0,0 +1,18 @@
+//
+//  ParticipationCell.swift
+//  AllIn
+//
+//  Created by Lucas Delanier on 06/06/2024.
+//
+
+import SwiftUI
+
+struct ParticipationCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ParticipationCell()
+}
